f860141f2c68391e74bbbcc65c671a1f04e416a4
bigdogawb <bigdogbaker734@gmail.com>
2026-01-21 12:21:22 -0500

    Attempt 2 to fix old version issue

--- plugins/constraints/src/main/kotlin/app/aaps/plugins/constraints/versionChecker/VersionCheckerPlugin.kt
+++ plugins/constraints/src/main/kotlin/app/aaps/plugins/constraints/versionChecker/VersionCheckerPlugin.kt
@@ -60,16 +60,16 @@ class VersionCheckerPlugin @Inject constructor(
             get() = TimeUnit.DAYS.toMillis(1)
     }
 
-    override fun isClosedLoopAllowed(value: Constraint<Boolean>): Constraint<Boolean> {
-        checkWarning()
-        versionCheckerUtils.triggerCheckVersion()
-        if (lastCheckOlderThan(gracePeriod.veryOld.daysToMillis()))
-            value.set(false, rh.gs(R.string.very_old_version), this)
-        val endDate = sp.getLong(rh.gs(app.aaps.core.utils.R.string.key_app_expiration) + "_" + config.VERSION_NAME, 0)
-        if (endDate != 0L && dateUtil.now() > endDate)
-            value.set(false, rh.gs(R.string.application_expired), this)
-        return value
-    }
+    // override fun isClosedLoopAllowed(value: Constraint<Boolean>): Constraint<Boolean> {
+    //     checkWarning()
+    //     versionCheckerUtils.triggerCheckVersion()
+    //     if (lastCheckOlderThan(gracePeriod.veryOld.daysToMillis()))
+    //         value.set(false, rh.gs(R.string.very_old_version), this)
+    //     val endDate = sp.getLong(rh.gs(app.aaps.core.utils.R.string.key_app_expiration) + "_" + config.VERSION_NAME, 0)
+    //     if (endDate != 0L && dateUtil.now() > endDate)
+    //         value.set(false, rh.gs(R.string.application_expired), this)
+    //     return value
+    // }
 
     override fun applyMaxIOBConstraints(maxIob: Constraint<Double>): Constraint<Double> =
         if (lastCheckOlderThan(gracePeriod.old.daysToMillis()))
